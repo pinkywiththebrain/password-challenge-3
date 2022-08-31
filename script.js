ed1991b02a29ddd9fdecd4d2211a4fb52a0ae80c
Sophie Carr <sophiemc99@gmail.com>
2022-08-31 16:40:19 -0400

    added randomization

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 //Targets the generate id (the button)
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(userLength) {
+function randomNum(min, max) {
+  if (!max) {
+    max - min
+    min - 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand) + rand*max)
+}
+
+function randomItem(list) {
+  return list[randomNum(list.length)]
+}
+
+function generatePassword() {
   //1. Prompt user for password criteria 
   //    a. length between 8 and 128 characters
   //    b. lowercase, uppercase, numbers, and special characters
 
-  //Length
+  //Length Prompt
   var userLength = window.prompt("How long would you like your password to be? (Please choose between 8 and 128 characters)");
 
   if (isNaN(userLength)) {
@@ -19,56 +32,63 @@ function generatePassword(userLength) {
     return;
   }
 
-console.log(userLength)
+  console.log(userLength)
 
-  //Uppercase
+  // The array that will store the chosen arrays
+  var selected = []
+  console.log(selected)
+
+  //Uppercase Prompt
   var upLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
   var userUpper = window.confirm("Press OK if you want to include uppercase letters.")
     if (userUpper) {
-      
+      selected.push(upLetters);
     } else if (!userUpper) {
 
     }
 
-  //Lowercase
+  //Lowercase Prompt
   var lowLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
   var userLower = window.confirm("Press OK if you want to include lowercase letters.")
     if (userLower) {
-
+      selected.push(lowLetters);
     } else if (!userLower) {
 
     }
 
-//Numbers
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+  //Numbers Prompt
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
   var userNum = window.confirm("Press OK if you want to include numbers.")
     if (userNum) {
-      
+      selected.push(numbers);
     } else if (!userNum) {
 
     }
 
-//Special Characters
+  //Special Characters Prompt
   var specialChars = ["!","@","#","$","%","^","&","*","?"]
 
   var userChars = window.confirm("Press OK if you want to include special characters.")
     if (userChars) {
-      
+      selected.push(specialChars);
     } else if (!userChars) {
 
     }
 
-  //2. Validate input
 
+    var generatedPassword = ""
 
-  //3. Generate password based on criteria
-//  var index = 
-//  var password = options[index];
+    for (var i = 0; i < userLength; i++) {
+      var randomList = randomItem(selected)
+      var randomChar = randomItem(randomList)
+      console.log(randomChar)
+    }
 
-  //4. Display generate pw on page
+    
+  
   return
 }
 
